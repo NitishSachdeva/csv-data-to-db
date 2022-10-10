3f5d341e0534ae4a7f7b5ca9d251a2d21ba28551
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-10-11 00:21:28 +0530

    Project Template

---
+++ src/main/java/com/example/csvdatatodb/CsvDataToDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.csvdatatodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvDataToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvDataToDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/csvdatatodb/CsvDataToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csvdatatodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvDataToDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
