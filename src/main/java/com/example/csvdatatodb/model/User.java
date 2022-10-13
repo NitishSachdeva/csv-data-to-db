95a6830ab2bf044981c3a2640a6fa9cba61ffd78
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-10-14 01:55:10 +0530

    Using Lombok in entity class

--- src/main/java/com/example/csvdatatodb/model/User.java
+++ src/main/java/com/example/csvdatatodb/model/User.java
@@ -1,53 +1,15 @@
 package com.example.csvdatatodb.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private long userId;
     private String namePrefix;
     private String firstName;
     private String lastName;
-
-    public User() {
-
-    }
-
-    public User(long userId, String namePrefix, String firstName, String lastName) {
-        this.userId = userId;
-        this.namePrefix = namePrefix;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
-
-    public String getNamePrefix() {
-        return namePrefix;
-    }
-
-    public void setNamePrefix(String namePrefix) {
-        this.namePrefix = namePrefix;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-
 }
